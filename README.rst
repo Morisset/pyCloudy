6c192fddd5ca00ca538980264a0cff4084b23797
morisset <chris.morisset@gmail.com>
2015-10-12 21:31:28 -0300

    add a pyCloudy level

---
+++ README.rst
@@ -0,0 +1,38 @@
+PyCloudy is a Python library to deal with input and output files of Cloudy (Gary Ferland) photoionization code.
+Is also allows to generate 3D nebula from various runs of the 1D Cloudy code.
+
+Requirements
+============
+
+You will need to have installed `Cloudy <http://nublado.org/>`_ .
+PyCloudy uses numpy and other standard python libraries.
+
+Installation
+============
+
+You may find useful to download, install and upgrade pyCloudy using
+`pip <http://www.pip-installer.org/en/latest/index.html>`_ .
+
+For example:
+
+* pip install --user pyCloudy
+
+Updates use the same command. You can also use 
+
+* easy_install --user pyCloudy
+
+Warranty
+========
+
+PyCloudy is provided as it is. No Warranty at all, you cannot complain if it runs models which are not fitting your data! :-)
+
+Manual
+======
+
+* There is no manual for now... There will soon be a HowTo page.
+
+* But... there is some examples of use in the `pyCloudy home page
+  <https://sites.google.com/site/pycloudy/>`_ .
+
+* And the reference manual is accessible from this site `HERE
+  <http://packages.python.org/pyCloudy/>`_ .
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import pyCloudy
+
+setup(name='pyCloudy',
+      version=pyCloudy.__version__,
+      description='Tools to manage Astronomical Cloudy photoionization code',
+      long_description=open('README.rst').read(),
+      author='Christophe Morisset',
+      author_email='chris.morisset@gmail.com',
+      maintainer='Christophe Morisset',
+      maintainer_email='chris.morisset@gmail.com',
+      url='https://sites.google.com/site/pycloudy/',
+      download_url = 'https://github.com/Morisset/pyCloudy/releases/tag/{}'.format(version),
+      packages=['pyCloudy','pyCloudy.c1d','pyCloudy.c3d','pyCloudy.utils', 'pyCloudy.db'],
+      package_data={'pyCloudy.utils':['*txt']},
+      classifiers=['Development Status :: 0',
+                   'Intended Audience :: Science/Research',
+                   'License :: Free for non-commercial use',
+                   'Programming Language :: Python :: 2',
+                   'Topic :: Scientific/Engineering :: Astronomy'
+                   ],
+      license='GPL',
+      keywords="astronomy photoionization cloudy"
+     )
