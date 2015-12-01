075342f7d33884c8f536e3b461aa5ce5204c2ef7
Morisset <chris.morisset@gmail.com>
2015-12-01 12:19:40 -0600

    correcting another bug in set_fudge

--- pyCloudy/c1d/cloudy_model.py
+++ pyCloudy/c1d/cloudy_model.py
@@ -1894,9 +1894,10 @@ class CloudyInput(object):
         """
         if fudge_params is None:
             self._fudge = None
+            return None
         elif type(fudge_params) != type(()) and type(fudge_params) != type([]):
             fudge_params = [fudge_params]
-            self._fudge = 'fudge factors ' + ' , '.join([str(fudge_param) for fudge_param in fudge_params])
+        self._fudge = 'fudge factors ' + ' , '.join([str(fudge_param) for fudge_param in fudge_params])
 
     def set_sphere(self, sphere = True):
         """
