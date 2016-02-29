10bfe41b369743f9eba0631193b8dd8c75efaf22
morisset <chris.morisset@gmail.com>
2016-02-29 10:12:36 -0600

    __version__ location
    changing the location of __version__. It is now in a separate file. It
    allows a simpler setup (no importing all pyCloudy for only __version__)

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import pyCloudy
+from pyCloudy.version import __version__
 
 setup(name='pyCloudy',
-      version=pyCloudy.__version__,
+      version=__version__,
       description='Tools to manage Astronomical Cloudy photoionization code',
       long_description=open('README').read(),
       author='Christophe Morisset IA-UNAM',
@@ -12,7 +12,7 @@ setup(name='pyCloudy',
       maintainer='Christophe Morisset IA-UNAM',
       maintainer_email='chris.morisset@gmail.com',
       url='https://sites.google.com/site/pycloudy/',
-      download_url = 'https://github.com/Morisset/pyCloudy/releases/tag/{}'.format(pyCloudy.__version__),
+      download_url = 'https://github.com/Morisset/pyCloudy/releases/tag/{}'.format(__version__),
       packages=['pyCloudy','pyCloudy.c1d','pyCloudy.c3d','pyCloudy.utils', 'pyCloudy.db'],
       package_data={'pyCloudy.utils':['*txt']},
       classifiers=['Development Status :: 5 - Production/Stable',
